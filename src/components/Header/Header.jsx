168822ab7f09218c763493234d76e811016f2d66
MaxatDzhetenbaev <yaboku18@mail.ru>
2024-02-16 17:14:26 +0300

    feat: Добавлена секция контакты в компоненте Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import './Hedaer.css'
 
 export const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <header className='header'>
+            <section className="header__contact">
+                <h3>Наш телефон:</h3>
+                <a href="tel:+996705188955">+996 705 188 955</a>
+                <a href="tel:+996705188955">+996 705 188 955</a>
+                <section className="header__working_hous">
+
+                </section>
+            </section>
+        </header>
+    )
 }
