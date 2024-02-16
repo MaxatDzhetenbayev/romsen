0ee5dd24fe173676178fae47711b5ce2e5b37bfc
MaxatDzhetenbaev <yaboku18@mail.ru>
2024-02-16 16:08:56 +0300

    style: Добавлены глбальные стили.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { MainPage } from './pages/Main/MainPage.jsx'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BaseLayout } from './layouts/BaseLayout.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    element: <BaseLayout/>,
+    children: [
+      {
+        path: '/',
+        element: <MainPage/>
+      }
+    
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}>
+
+    </RouterProvider>
   </React.StrictMode>,
 )
