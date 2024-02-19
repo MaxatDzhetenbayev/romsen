ae0d96b302f48e970af09c93589fc9dbc380ed77
MaxatDzhetenbaev <yaboku18@mail.ru>
2024-02-19 16:43:28 +0300

    ref: малые изменения в header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,17 @@
 import styles from './Hedaer.module.css'
-
 export const Header = () => {
     return (
         <header className={styles.header}>
-            <section className="header__contact">
+            <section className={styles.header__contact}>
                 <h3>Наш телефон:</h3>
                 <a href="tel:+996705188955">+996 705 188 955</a>
                 <a href="tel:+996705188955">+996 705 188 955</a>
-                <section className="header__working_hous">
+                <section className={styles.header__working_hous}>
                     <img src="svgs/time.svg" alt="" />
                     <p>работаем с 10:00 до 00:00</p>
                 </section>
             </section>
-            <section>
+            <section className={styles.header__right}>
                 <section>
                     <p>Город:</p>
                     <h3>Казахстан</h3>
