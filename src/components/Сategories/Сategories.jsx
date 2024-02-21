de487614c83617d59500d9f0a8307e600efbe559
MaxatDzhetenbaev <yaboku18@mail.ru>
2024-02-21 12:57:59 +0300

    fix: Изменен неправильный тег img

--- src/components/Сategories/Сategories.jsx
+++ src/components/Сategories/Сategories.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
-import './Categories.module.css'
-import Chicken from "./pictures/chicken.png";
+import styles from './Categories.module.css'
+import Chicken from "./pictures/chiken.png";
 import Corndog from "./pictures/corndog.png";
 import Eel from "./pictures/eel.png";
 import Stocks from "./pictures/stocks.png";
@@ -9,13 +8,13 @@ import Pizza from "./pictures/pizza.png";
 export const Categories = () => {
     return (
        <section className={styles.wrapper}>
-        <div className={styles.chiken}>
+        <div className={styles.chicken}>
             <h4>Чикен</h4>
-            <image src= {Chicken}></image>
+            <img src= {Chicken}></img>
         </div>
         <div className={styles.eel}>
              <h4>C угрем</h4>
-            <image src= {Eel}></image>
+            <img src= {Eel}></img>
         </div>
         <div className={styles.corndog}>
             <h4>Корндог</h4>
@@ -23,11 +22,11 @@ export const Categories = () => {
         </div>
         <div className={styles.pizza}>
             <h4>Пицца</h4>
-            <image src= {Pizza}></image>
+            <img src= {Pizza}></img>
         </div>
         <div className={styles.stocks}>
             <h4>Акции</h4>
-            <image src= {Stocks}></image>
+            <img src= {Stocks}></img>
         </div>
        </section>
     )
