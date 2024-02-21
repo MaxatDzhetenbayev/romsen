86ca9b43849204e51e96f69e65fa7e2e3de97b96
MaxatDzhetenbaev <yaboku18@mail.ru>
2024-02-21 14:57:46 +0300

    ref: Неполный рефактор компоненты new product

--- src/components/NewProducts/NewProducts.jsx
+++ src/components/NewProducts/NewProducts.jsx
@@ -1,51 +1,53 @@
-import React from 'react'
-import './NewProducts.css'
+import { Card } from '../ui/Card/Card'
+import style from './NewProducts.module.css'
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+
+// import required modules
+import { Navigation } from 'swiper/modules';
+
+const newProductList = [
+    {
+        name: "Саламон сет",
+        desc: "1050 грамм, 30 кусочков",
+        price: 6500,
+        img: "newProducts/salomon.png"
+    },
+    {
+        name: "Филадельфия и лосось сет",
+        desc: "1260 грамм, 36 кусочков",
+        price: 6900,
+        img: "newProducts/philadelphia.png"
+
+    },
+    {
+        name: "Самая большая Филадельфия",
+        desc: "2050 грамм, 45 кусочков",
+        price: 9000,
+        img: "newProducts/bigPhiladelphia.png"
+    },
+]
+
+
 
 export const NewProducts = () => {
     return (
-        <>
-            <section className='newProds'>
-                <article className='prod'>
-                    <section style={{
-                        borderBottom: '1px solid #A4ACAD',
-                        padding: "10px"
+        <Swiper navigation={true} modules={[Navigation]} className={style.new_products} slidesPerView={2}
+            spaceBetween={30} >
+            {newProductList.map((item) => (
+                <SwiperSlide
+                className={style.product_item}
+                    key={item.desc}>
+                    <Card  {...item} />
+                </SwiperSlide>
+            ))}
 
-                    }}>
-                        <img src="/public/NewProduct/salomon.png" alt="" />
-                        <h3 className='prodMainText'>Саломон сет</h3>
-                        <p className='prodText'>??? грамм, ??? кусочков</p>
-                    </section>
-                    <h3 className='prodBottom'>
-                        ??? СОМ
-                    </h3>
-                </article>
+        </Swiper>
 
-                <div className='prod'>
-                    <div className='prodPhoto'>
-                        <div><img src="/public/NewProduct/philadelphia.png" alt="" /></div>
-                        <h3 className='prodMainText'>Филадельфия сет</h3>
-                        <p className='prodText'>??? грамм, ??? кусочков</p>
-                    </div>
-                    <h3 className='prodBottom'>
-                        ??? СОМ
-                    </h3>
-                </div>
-
-                <div className='prod'>
-                    <div className='prodPhoto'>
-                        <div><img src="/public/NewProduct/bigPhiladelphia.png" alt="" /></div>
-                        <h3 className='prodMainText'>Самая большая Филадельфия</h3>
-                        <p className='prodText'>??? грамм, ??? кусочков</p>
-                    </div>
-                    <h3 className='prodBottom'>
-                        ??? СОМ
-                    </h3>
-                </div>
-            </section>
-
-
-
-
-        </>
     )
 }
