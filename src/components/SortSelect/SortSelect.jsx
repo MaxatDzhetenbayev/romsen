0fdc093a8abc62983342712deb6c42c927e039e9
iufb <misakilover228@gmail.com>
2024-02-26 17:26:20 +0600

    feat: sort select ui

---
+++ src/components/SortSelect/SortSelect.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+const sortOptions = [
+  { value: "base", label: "По умолчанию" },
+  { value: "name", label: "Название" },
+  { value: "price-low", label: "Сначала дешевле" },
+  { value: "price-up", label: "Сначала дороже" },
+  { value: "pieces", label: "Количество кусочков" },
+  { value: "weight", label: "Вес" },
+];
+import styles from "./SortSelect.module.css";
+import clsx from "clsx";
+
+export const SortSelect = ({ products, sortProduct }) => {
+  const [opened, setOpened] = useState(false);
+  const [sortType, setSortType] = useState(sortOptions[0]);
+  const changeSort = (sort) => {
+    setSortType(sort);
+    setOpened(false);
+  };
+  return (
+    <div className={clsx(styles.wrapper, opened && styles.shadow)}>
+      <label htmlFor="select" className={styles.title}>
+        Сортировка
+      </label>
+      <ul
+        id="select"
+        className={styles.select}
+        value={sortType}
+        onChange={(e) => setSortType(e.target.value)}
+      >
+        <div
+          className={styles.selected}
+          onClick={() => setOpened((prev) => !prev)}
+        >
+          <li>{sortType.label} </li>
+          <img src="/arrow-down.svg" />
+        </div>
+        {opened && (
+          <>
+            <hr className={styles.divider} />
+            {sortOptions.map((sort) => (
+              <li className={styles.option} onClick={() => changeSort(sort)}>
+                {sort.label}
+              </li>
+            ))}
+          </>
+        )}
+      </ul>
+    </div>
+  );
+};
