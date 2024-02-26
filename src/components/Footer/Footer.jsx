96ec08b1517da1ff164f4aad9a9f34a57ab1ef2e
Magzhan K <kasymmaks31@gmail.com>
2024-02-26 16:51:38 +0600

    Доделан футер

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import styles from './Footer.module.css'
 import { Link } from 'react-router-dom'
 
+
+
+
+
 export const Footer = () => {
     return (
         <>
@@ -16,12 +20,19 @@ export const Footer = () => {
                 <div className={styles.content}>
                     <p className={styles.content_paragraph}>Введите номер</p>
                     <p>8 ___-___-__-__</p>
+                    <p> </p>
                     <p>Выберите удобный мессенджер для общения</p>
+                    <section className={styles.socials}>
+                    <Link to="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" target='blank'><img src="footer/whatsapp.png"alt="" /></Link>
+                    <Link to="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" target='blank'><img src="footer/instagram.png"alt="" /></Link>
+                    <Link to="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" target='blank'><img src="footer/telegram.png"alt="" /></Link>
+
+                    </section>
                 </div>
 
                 <div className={styles.content}>
-                    <p>Тел: 87077777474</p>
-                    <p>Тел: 87070007474</p>
+                    <p className='underlined'>Тел: 87077777474</p>
+                    <p className='underlined'>Тел: 87070007474</p>
                     <p>Адрес: Бакаева 126</p>
                 </div>
 
