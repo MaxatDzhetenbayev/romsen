e73cb53cc57209c7e562acfc94a697401e2b0cdb
MaxatDzhetenbaev <yaboku18@mail.ru>
2024-02-28 13:54:36 +0300

    ref: Неоконченные стили для новых продуктов

--- src/components/NewProducts/NewProducts.jsx
+++ src/components/NewProducts/NewProducts.jsx
@@ -1,7 +1,7 @@
-import { Card } from '../ui/Card/Card'
+// import { Card } from '../ui/Card/Card'
 import style from './NewProducts.module.css'
 import { Swiper, SwiperSlide } from 'swiper/react';
-
+import 'swiper/css/free-mode';
 
 // Import Swiper styles
 import 'swiper/css';
@@ -9,7 +9,7 @@ import 'swiper/css/navigation';
 
 
 // import required modules
-import { Navigation } from 'swiper/modules';
+import { FreeMode, Navigation } from 'swiper/modules';
 
 const newProductList = [
     {
@@ -31,23 +31,51 @@ const newProductList = [
         price: 9000,
         img: "newProducts/bigPhiladelphia.png"
     },
+    {
+        name: "Филадельфия и лосось сет",
+        desc: "1260 грамм, 36 кусочков",
+        price: 6900,
+        img: "newProducts/philadelphia.png"
+    },
+    {
+        name: "Саламон сет",
+        desc: "1050 грамм, 30 кусочков",
+        price: 6500,
+        img: "newProducts/salomon.png"
+    },
 ]
 
 
 
 export const NewProducts = () => {
     return (
-        <Swiper navigation={true} modules={[Navigation]} className={style.new_products} slidesPerView={2}
-            spaceBetween={30} >
-            {newProductList.map((item) => (
-                <SwiperSlide
-                className={style.product_item}
-                    key={item.desc}>
-                    <Card  {...item} />
-                </SwiperSlide>
-            ))}
+        <section className='container__small'>
+            <Swiper
+                navigation={true}
+                modules={[Navigation, FreeMode]}
+                className={style.new_products}
+                slidesPerView={3}
+                spaceBetween={10}
+                autoHeight={true}
+            >
+                {newProductList.map((item) => (
+                    <SwiperSlide
+                        key={item.desc}>
+                        <article className={style.product_item}>
+                            <img src={item.img} alt={item.name} />
+                        <header>
+                            <h1>{item.name}</h1>
+                            <p>{item.desc}</p>
+                        </header>
+                        <footer>
+                            <p>{item.price}</p>
+                            <button>Хочу!</button>
+                        </footer>
+                        </article>
+                    </SwiperSlide>
+                ))}
 
-        </Swiper>
-
+            </Swiper>
+        </section>
     )
 }
