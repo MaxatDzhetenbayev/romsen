6a492b816930e739e220bebeb5e3c8ce96317d47
Magzhan K <kasymmaks31@gmail.com>
2024-03-01 17:48:59 +0600

    feat: Добавлен меню моб версии

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import styles from './Menu.module.css'
+
+export const Menu = () => {
+  return (
+    <>
+    <div class={styles.menu_categories}>
+    <img src="menu/pizza.png" alt="" />
+    <img src="menu/sets.png" alt="" />
+    <img src="menu/wok.png" alt="" />
+    <img src="menu/rolls.png" alt="" />
+    <img src="menu/sushi.png" alt="" />
+    <img src="menu/soup.png" alt="" />
+    <img src="menu/salads.png" alt="" />
+    <img src="menu/drinks.png" alt="" />
+    </div>
+
+
+
+
+
+
+
+
+
+    </>
+  )
+}
