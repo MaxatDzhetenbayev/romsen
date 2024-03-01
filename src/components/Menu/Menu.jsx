35e9fd085d2edc7dcd7997c53f43ccbabeb9c93b
MaxatDzhetenbaev <yaboku18@mail.ru>
2024-03-01 17:12:00 +0500

    fix: декоротивные изменения в компоненте menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,36 +1,35 @@
-import React from 'react'
 import styles from './Menu.module.css'
 import { Link } from 'react-router-dom'
 
 export const Menu = () => {
   return (
     <>
-    <div class={styles.menu_categories}>
-    <Link>
-    <img src="menu/pizza.png" alt="" />
-    </Link>
-    <Link>
-    <img src="menu/sets.png" alt="" />
-    </Link>
-    <Link>
-    <img src="menu/wok.png" alt="" />
-    </Link>
-    <Link>
-    <img src="menu/rolls.png" alt="" />
-    </Link>
-    <Link>
-    <img src="menu/sushi.png" alt="" />
-    </Link>
-    <Link>
-    <img src="menu/soup.png" alt="" />
-    </Link>
-    <Link>
-    <img src="menu/salads.png" alt="" />
-    </Link>
-    <Link>
-    <img src="menu/drinks.png" alt="" />
-    </Link>
-    </div>
+      <div className={styles.menu_categories}>
+        <Link>
+          <img src="menu/pizza.png" alt="" />
+        </Link>
+        <Link>
+          <img src="menu/sets.png" alt="" />
+        </Link>
+        <Link>
+          <img src="menu/wok.png" alt="" />
+        </Link>
+        <Link>
+          <img src="menu/rolls.png" alt="" />
+        </Link>
+        <Link>
+          <img src="menu/sushi.png" alt="" />
+        </Link>
+        <Link>
+          <img src="menu/soup.png" alt="" />
+        </Link>
+        <Link>
+          <img src="menu/salads.png" alt="" />
+        </Link>
+        <Link>
+          <img src="menu/drinks.png" alt="" />
+        </Link>
+      </div>
 
 
 
