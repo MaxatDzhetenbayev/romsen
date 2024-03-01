2d286a763360ee14f7c4e862d0a7a94cf00f9a73
iufb <misakilover228@gmail.com>
2024-03-01 17:04:51 +0600

    ref: provide classes to aside and sidebar, delete wrappers

--- src/layouts/BaseLayout.jsx
+++ src/layouts/BaseLayout.jsx
@@ -1,32 +1,23 @@
-import { Outlet } from "react-router-dom"
-import { Aside } from "../components/Aside/Aside"
-import Sidebar from "../components/Sidebar/Sidebar"
-import { Header } from "../components/Header/Header"
-import styles from './BaseLayouts.module.css'
-import { Footer } from "../components/Footer/Footer"
-import { TextAbout } from "../components/TextAbout/TextAbout"
+import { Outlet } from "react-router-dom";
+import { Aside } from "../components/Aside/Aside";
+import Sidebar from "../components/Sidebar/Sidebar";
+import { Header } from "../components/Header/Header";
+import styles from "./BaseLayouts.module.css";
+import { Footer } from "../components/Footer/Footer";
+import { TextAbout } from "../components/TextAbout/TextAbout";
 
 export const BaseLayout = () => {
-    return (
-        <div className={styles.base_layout}>
-            <div className={styles.elems_to_hide}>
-                <Sidebar />
-            </div>
+  return (
+    <div className={styles.base_layout}>
+      <Sidebar className={styles.elems_to_hide} />
 
-            <p className={styles.main}>
-                <Header />
-                <Outlet />
-                <TextAbout />
-                <Footer />
-            </p>
-            <div className={styles.elems_to_hide}>
-                <Aside />
-            </div>
-
-        </div>
-    )
-}
-
-
-
-
+      <p className={styles.main}>
+        <Header />
+        <Outlet />
+        <TextAbout />
+        <Footer />
+      </p>
+        <Aside  className={styles.elems_to_hide}/>
+    </div>
+  );
+};
