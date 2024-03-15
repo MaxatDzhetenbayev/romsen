a8996ffe92828daa6cb6c709e750c6375e439c89
iufb <misakilover228@gmail.com>
2024-03-15 22:49:17 +0600

    feat: added links to menu

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -2,17 +2,22 @@ import React from "react";
 import { useContext } from "react";
 import { CartContext } from "../../context/cart.context";
 import styles from "./MobileMenu.module.css";
+import { Link } from "react-router-dom";
 export const MobileMenu = () => {
   const { openCart, open, closeCart } = useContext(CartContext);
   return (
     <div className={styles.wrapper}>
-      <MenuButton name="Меню" icon="/svgs/menu-icon.svg" />
+      <Link to={"/"}>
+        <MenuButton name="Меню" icon="/svgs/menu-icon.svg" />
+      </Link>
       <MenuButton
         onClick={open ? closeCart : openCart}
         name="Корзина"
         icon="/svgs/cart-icon.svg"
       />
-      <MenuButton name="Отзывы" icon="/svgs/review-icon.svg" />
+      <Link to={"/reviews"}>
+        <MenuButton name="Отзывы" icon="/svgs/review-icon.svg" />
+      </Link>
     </div>
   );
 };
