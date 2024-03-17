28a2e281bcc2298bdcd7e9a60436df6c1c2e02dc
iufb <misakilover228@gmail.com>
2024-03-17 11:32:47 +0500

    feat: payment page

--- src/pages/PaymentPage/PaymentPage.jsx
+++ src/pages/PaymentPage/PaymentPage.jsx
@@ -1,11 +1,28 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+
+import { Counter } from "../../components/Counter/Counter";
+import { Cart } from "../../components/Cart/Cart";
 import { Input } from "../../components/ui/Input/Input";
 import { Button } from "../../components/ui/Button/Button";
 import styles from "./PaymentPage.module.css";
+import clsx from "clsx";
+import { CartContext } from "../../context/cart.context";
+import { CartItem } from "../../components/Cart/CartItem/CartItem";
 
 export const PaymentPage = () => {
+  const { totalPrice } = useContext(CartContext);
   const [isCardMethod, setIsCardMethod] = useState(false);
   const [delivery, setDelivery] = useState("courier");
+  const [now, setNow] = useState("now");
+  const [count, setCount] = useState(0);
+  const increase = () => setCount(count + 1);
+  const decrease = () =>
+    setCount((prev) => {
+      if (prev == 0) {
+        return 0;
+      }
+      return prev - 1;
+    });
 
   return (
     <section className={styles.container}>
@@ -25,30 +42,42 @@ export const PaymentPage = () => {
             <Input placeholder="Телефон" />
             <Input placeholder="Имя" />
           </section>
-          <section className={styles.split}>
+          <section className={clsx(styles.split, styles.delivery)}>
             <button
               onClick={() => setDelivery("courier")}
-              className={delivery == "courier" && styles.splitSelected}
+              className={clsx(
+                styles.splitButton,
+                delivery == "courier" && styles.splitSelected,
+              )}
             >
               Курьером
             </button>
             <button
               onClick={() => setDelivery("self")}
-              className={delivery == "self" && styles.splitSelected}
+              className={clsx(
+                styles.splitButton,
+                delivery == "self" && styles.splitSelected,
+              )}
             >
               Самовывоз
             </button>
           </section>
-          <section className={styles.split}>
+          <section className={clsx(styles.split, styles.paymentType)}>
             <button
               onClick={() => setIsCardMethod(true)}
-              className={isCardMethod && styles.splitSelected}
+              className={clsx(
+                styles.splitButton,
+                isCardMethod && styles.splitSelected,
+              )}
             >
               Наличными
             </button>
             <button
               onClick={() => setIsCardMethod(false)}
-              className={!isCardMethod && styles.splitSelected}
+              className={clsx(
+                styles.splitButton,
+                !isCardMethod && styles.splitSelected,
+              )}
             >
               Картой
             </button>
@@ -57,9 +86,96 @@ export const PaymentPage = () => {
             <Input placeholder="Улица" />
             <Input placeholder="Дом" />
           </section>
+          <section className={styles.change}>
+            <div className={styles.checkbox}>
+              <input type="checkbox" />
+              <span>Подготовить сдачу с</span>
+            </div>
+            <Input placeholder="Сумма" />
+          </section>
+          <section className={styles.userHome}>
+            <Input placeholder="Кв" />
+            <Input placeholder="Подъезд" />
+            <Input placeholder="Этаж" />
+            <Input placeholder="Код" />
+          </section>
+          <section className={styles.comment}>
+            <Input placeholder="Комменатрий к заказу" />
+          </section>
+          <section className={clsx(styles.split, styles.time)}>
+            <button
+              onClick={() => setNow("now")}
+              className={clsx(
+                styles.splitButton,
+                now == "now" && styles.splitSelected,
+              )}
+            >
+              На сейчас
+            </button>
+            <button
+              onClick={() => setNow("soon")}
+              className={clsx(
+                styles.splitButton,
+                now == "soon" && styles.splitSelected,
+              )}
+            >
+              На время
+            </button>
+          </section>
+          <section className={styles.extra}>
+            <span>Палочки + соусник обычные</span>
+            <Counter quantity={count} increase={increase} decrease={decrease} />
+          </section>
+          <section className={styles.email}>
+            <Input placeholder="E-mail(необязательно)" />
+          </section>
+          <section className={styles.promo}>
+            <Input placeholder="Введите промокод" />
+          </section>
+          <section className={styles.total}>
+            <section className={styles.between}>
+              <span>Доставка</span>
+              <span>Бесплатно</span>
+            </section>
+            <section className={styles.between}>
+              <span>Итого</span>
+              <span>{totalPrice} ТЕНГЕ</span>
+            </section>
+          </section>
+
+          <Button color="orange" className={styles.submit}>
+            Оформить заказ
+          </Button>
+          <span className={styles.permissions}>
+            Нажимая на кнопку Оформить заказ, Вы подтверждаете свое согласие на
+            обработку персональных данных в соответствии с Публичной оффертой
+          </span>
         </section>
       </section>
-      <section className={styles.cart}>Корзина</section>
+      <CartContainer />
+    </section>
+  );
+};
+
+const CartContainer = () => {
+  const { products, totalPrice } = useContext(CartContext);
+  return (
+    <section className={styles.cart}>
+      <section className={styles.cartItems}>
+        {products.map((product) => (
+          <CartItem product={product} />
+        ))}
+      </section>
+      <section className={styles.cartBottom}>
+        <section className={styles.between}>
+          <span>Доставка</span>
+          <span>Бесплатно</span>
+        </section>
+        <section className={styles.between}>
+          <span>Итого</span>
+          <span>{totalPrice} ТЕНГЕ</span>
+        </section>
+      </section>
     </section>
   );
 };
--- src/pages/Reviews/ReviewsPage.jsx
+++ src/pages/Reviews/ReviewsPage.jsx
@@ -1,41 +1,43 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios'
-import styles from './ReviewsPage.module.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import styles from "./ReviewsPage.module.css";
 
 export const ReviewsPage = () => {
   const initialReviews = [
     {
-      username: 'Розалия',
-      date: '24.02.2021',
-      text: 'Ваша доставка и ваши блюда лучшие в Харькове!) всегда очень вкусно, вовремя, всегда вежливые курьеры и девушки на телефоне',
+      username: "Розалия",
+      date: "24.02.2021",
+      text: "Ваша доставка и ваши блюда лучшие в Харькове!) всегда очень вкусно, вовремя, всегда вежливые курьеры и девушки на телефоне",
     },
     {
-      user: 'Елена',
-      date: '23.02.2021',
-      text: 'Ооочень вкусно!!!!!',
+      user: "Елена",
+      date: "23.02.2021",
+      text: "Ооочень вкусно!!!!!",
     },
     {
-      user: 'Сергей Гаврилюк',
-      date: '23.02.2021',
-      text: 'Заказываем у Вас больше 2-ух лет, были разные ситуации, но сервис стал лучше, суши вкуснее. За доставку сегодня на время, огромное спасибо, точь-в-точь в минуту в минуту. Успехов Вам и приятных бонусов нам )',
+      user: "Сергей Гаврилюк",
+      date: "23.02.2021",
+      text: "Заказываем у Вас больше 2-ух лет, были разные ситуации, но сервис стал лучше, суши вкуснее. За доставку сегодня на время, огромное спасибо, точь-в-точь в минуту в минуту. Успехов Вам и приятных бонусов нам )",
     },
   ];
 
   const [reviews, setReviews] = useState(initialReviews);
-  const [newReview, setNewReview] = useState('');
-  const [username, setUsername] = useState('');
+  const [newReview, setNewReview] = useState("");
+  const [username, setUsername] = useState("");
   const [showForm, setShowForm] = useState(false);
-  
+
   useEffect(() => {
     fetchReviews();
-  }, []); 
+  }, []);
 
   const fetchReviews = async () => {
     try {
-      const response = await axios.get('https://65e830004bb72f0a9c4e817e.mockapi.io/api/v1/comments');
+      const response = await axios.get(
+        "https://65e830004bb72f0a9c4e817e.mockapi.io/api/v1/comments",
+      );
       setReviews(response.data);
     } catch (error) {
-      console.error('Ошибка при загрузке отзывов:', error);
+      console.error("Ошибка при загрузке отзывов:", error);
     }
   };
 
@@ -51,8 +53,8 @@ export const ReviewsPage = () => {
         ...reviews,
       ];
       setReviews(newReviews);
-      setNewReview('');
-      setUsername('');
+      setNewReview("");
+      setUsername("");
       setShowForm(false);
     }
   };
@@ -60,42 +62,46 @@ export const ReviewsPage = () => {
   return (
     <div>
       <div className={styles.wrapper}>
-      <h1>Отзывы</h1>
-      <button className={styles.button} onClick={toggleForm}>
-        + Добавить отзыв
-      </button>
+        <h1>Отзывы</h1>
+        <button className={styles.button} onClick={toggleForm}>
+          + Добавить отзыв
+        </button>
       </div>
-      
+
       {showForm && (
         <div className={styles.form}>
           <input
+            className={styles.input}
             type="text"
             placeholder="Ваше имя"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
-           <textarea
+          <textarea
+            className={styles.input}
             placeholder="Введите ваш отзыв"
             value={newReview}
             onChange={(e) => setNewReview(e.target.value)}
           />
-          <button className={styles.button_form} onClick={addReview}>Добавить </button>
+          <button className={styles.button_form} onClick={addReview}>
+            Добавить{" "}
+          </button>
         </div>
       )}
 
-    <div className={styles.reviews_container}> 
-      <div className={styles.reviews}>
-        {reviews.map((review, index) => (
-          <div key={index} className={styles.container}>
-            <div className={styles.head}>
-              <h1 className={styles.user}>{review.username}</h1>
-              <p className={styles.date}>{review.date}</p>
+      <div className={styles.reviews_container}>
+        <div className={styles.reviews}>
+          {reviews.map((review, index) => (
+            <div key={index} className={styles.container}>
+              <div className={styles.head}>
+                <h1 className={styles.user}>{review.username}</h1>
+                <p className={styles.date}>{review.date}</p>
+              </div>
+              <p className={styles.text}>{review.text}</p>
             </div>
-            <p className={styles.text}>{review.text}</p>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
     </div>
   );
 };
